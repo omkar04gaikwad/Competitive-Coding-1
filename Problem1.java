9c053f546fddf9465824351db5d8dd05de3e0c3a
omkar04gaikwad <58172164+omkar04gaikwad@users.noreply.github.com>
2025-05-18 23:58:57 -0700

    Completed both Questions in Java

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,45 @@
+/*
+ * Question : Given a sorted array arr[] of n-1 integers, these integers are in the range of 1 to n.
+ * There are no duplicates in the array.
+ * One of the integers is missing in the array.
+ * Write an efficient code to find the missing integer.
+ * Input : arr[] = [1, 2, 3, 4, 6, 7, 8] Output : 5
+ * Explanation: The missing integer in the above array is 5 
+ * Input : arr[] = [1, 2, 3, 4, 5, 6, 8, 9] Output : 7
+ * Explanation: The missing integer in the above array is 7 
+ * 
+ * Approach - The given array is in range of [1,n] where n = array length
+ * The Brute-Force approach is to calculate estimated and actual sum and subtract the remaining is the
+ * missing number.
+ * Time Complexity - O(n) Space Complexity - O(1)
+ * 
+ * Optimized Approach - do a binary search as the given array is sorted, we find the mid index using
+ * left and right pointer. mid = l + (r-l)/2. if nums[mid] == mid + 1 means that till mid the array is
+ * correctly sorted and at each index i the element  = i+1, hence reject the left array and go to right.
+ * else, reject the right array and go to the left.
+*/
 
+public class Problem1 {
+
+    static int missingNumber(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == mid + 1) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l + 1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums_1 = { 1, 2, 3, 4, 6, 7, 8 };
+        int[] nums_2 = { 1, 2, 3, 4, 5, 6, 8, 9 };
+        System.out.println(missingNumber(nums_1));
+        System.out.println(missingNumber(nums_2));
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,137 @@
+/*
+ * 
+ * Below is the solution to Design min Heap using Java Array list.
+ * Time and Space Complexity
+ * Operation        Time Complexity             Space Complexity
+ * push()               O(log n)                     O(1)
+ * pop()                O(log n)                     O(1)
+ * HeapifyUp()          O(log n)                     O(log n) (recursive)
+ * HeapifyDown()        O(log n)                     O(log n) (recursive)
+ * buildHeap()          O(n)                         O(1)
+ * getMin()             O(1)                         O(1)
+ * show()               O(n)                         O(1)
+ */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class MinHeap {
+    ArrayList<Integer> Heap = new ArrayList<>();
+    int size_;
+
+    MinHeap() {
+        size_ = -1;
+    }
+
+    void HeapifyUp(int idx) {
+        if (idx == 0) {
+            return;
+        }
+        int p_index = (idx - 1) / 2;
+        if (Heap.get(idx) < Heap.get(p_index)) {
+            int temp = Heap.get(p_index);
+            Heap.set(p_index, Heap.get(idx));
+            Heap.set(idx, temp);
+            HeapifyUp(p_index);
+        }
+    }
+
+    void HeapifyDown(int idx) {
+        int smallest = idx;
+        int left = (2 * idx) + 1;
+        int right = (2 * idx) + 2;
+        if ((left < size_) && (Heap.get(left) < Heap.get(smallest))) {
+            smallest = left;
+        }
+        if ((right < size_) && (Heap.get(right) < Heap.get(smallest))) {
+            smallest = right;
+        }
+        if (smallest != idx) {
+            int temp = Heap.get(idx);
+            Heap.set(idx, Heap.get(smallest));
+            Heap.set(smallest, temp);
+            HeapifyDown(smallest);
+        }
+    }
+
+    void push(int x) {
+        Heap.add(x);
+        size_ += 1;
+        if (size_ == 0) {
+            return;
+        }
+        HeapifyUp(size_);
+    }
+
+    int pop() {
+        int temp = Heap.get(0);
+        Heap.set(0, Heap.get(size_));
+        Heap.remove(size_);
+        HeapifyDown(0);
+        size_ -= 1;
+        return temp;
+    }
+
+    void show() {
+        int n = Heap.size();
+        for (int i = 0; i < n; i++) {
+            System.out.println(Heap.get(i));
+        }
+    }
+
+    boolean isEmpty() {
+        return size_ == -1;
+    }
+
+    int size() {
+        return size_;
+    }
+
+    int getMin() {
+        return Heap.get(0);
+    }
+
+    int[] buildHeap(int[] nums) {
+        int n = nums.length / 2;
+        for (int i = n; i >= 0; i--) {
+            down(i, nums);
+        }
+        return nums;
+    }
+
+    void down(int idx, int[] nums) {
+        int n = nums.length;
+        int s = idx;
+        int l = (2 * idx) + 1;
+        int r = (2 * idx) + 2;
+        if ((l < n) && (nums[l] < nums[s])) {
+            s = l;
+        }
+        if ((r < n) && (nums[r] < nums[s])) {
+            s = r;
+        }
+        if (s != idx) {
+            int temp = nums[s];
+            nums[s] = nums[idx];
+            nums[idx] = temp;
+            down(s, nums);
+        }
+    }
+}
+
+public class Problem2 {
+    public static void main(String[] args) {
+        MinHeap mH = new MinHeap();
+        mH.push(-5);
+        mH.push(-3);
+        mH.push(-8);
+        mH.push(-2);
+        mH.show();
+        System.out.println(mH.pop());
+        mH.show();
+        System.out.println(mH.pop());
+        mH.show();
+        int[] nums = { 8, 6, 5, 3, 2, 1 };
+        System.out.println(Arrays.toString(mH.buildHeap(nums)));
+    }
+}
